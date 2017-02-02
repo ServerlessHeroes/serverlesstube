bee88cdd95154f2249f37d3311e772dd86d894ba
Mike G Chambers <mikechambers@Mikes-MBP.gateway>
2017-02-02 20:55:07 +1000

    Updates in progress
    Env vars and new mapping methods

--- lab-3/lambda/user-profile/index.js
+++ lab-3/lambda/user-profile/index.js
@@ -1,31 +1,27 @@
 /**
  * Created by Peter Sbarski
- * Last Updated: 28/03/2016
+ * Updated by Mike Chambers
+ * Last Updated: 1/02/2017
+ *
+ * Required Env Vars:
+ * AUTH0_DOMAIN
  */
 'use strict';
 
 var request = require('request');
-var env = require('./config');
 
-function successResponse(message){
-  return {
-    httpStatusCode: 200,
-    body: JSON.stringify({'message' : message})
-  }
-}
-
-function errorResponse(status, message){
-  return {
-    httpStatusCode: status,
-    body: JSON.stringify({'message':message})
-  }
+function generateResponse(status, message){
+    return {
+      statusCode: status,
+      body: JSON.stringify({'message':message})
+    }
 }
 
 exports.handler = function(event, context, callback){
     var authToken = event.queryStringParameters.Authorization;
 
     if (!authToken) {
-      var response = errorResponse(400, 'AuthToken not found');
+      var response = generateResponse(400, 'AuthToken not found');
 
     	callback(null, response);
     	return;
@@ -38,7 +34,7 @@ exports.handler = function(event, context, callback){
     };
 
     var options = {
-        url: 'https://' +  env.AUTH0_DOMAIN + '/tokeninfo',
+        url: 'https://' +  process.env.AUTH0_DOMAIN + '/tokeninfo',
         method: 'POST',
         json: true,
         body: body
@@ -46,11 +42,11 @@ exports.handler = function(event, context, callback){
 
     request(options, function(error, response, body){
         if (!error && response.statusCode === 200) {
-            var response = successResponse(body);
+            var response = generateResponse(200, body);
 
             callback(null, response);
         } else {
-            var response = errorResponse(error);
+            var response = generateResponse(400, error);
 
             callback(null, response);
         }
